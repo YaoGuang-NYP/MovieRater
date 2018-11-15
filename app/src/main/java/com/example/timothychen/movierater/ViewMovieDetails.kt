6b0d710c4dc4b8132f26cff58296028d6f67d4d5
YaoGuang(NYP) <170942E@mymail.nyp.edu.sg>
2018-11-16 03:55:49 +0800

    2nd Commit:
    Created view for view movie details static page, edited so minor details in manifest file

---
+++ app/src/main/java/com/example/timothychen/movierater/ViewMovieDetails.kt
@@ -0,0 +1,19 @@
+package com.example.timothychen.movierater
+
+import android.support.v7.app.AppCompatActivity
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ViewMovieDetails: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_movie_details)
+
+    }
+}
